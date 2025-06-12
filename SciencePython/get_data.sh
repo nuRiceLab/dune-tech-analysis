8810a464d32470d18e97abb0e0db89eb19b4e931
Matt Kramer <mjkramer@users.noreply.github.com>
2025-06-12 16:10:57 -0500

    Update get_data.sh

--- SciencePython/get_data.sh
+++ SciencePython/get_data.sh
@@ -3,6 +3,8 @@
 mkdir -p data
 cd data
 
+wget https://portal.nersc.gov/project/dune/data/2x2/people/mkramer/DUNE-TECH-2025/packet-0050017-2024_07_09_00_04_33_CDT.FLOW.hdf5
+
 wget https://portal.nersc.gov/project/dune/data/2x2/people/mkramer/DUNE-TECH-2025/stage2.pbp.eh1.7ad.root
 
 wget https://portal.nersc.gov/project/dune/data/2x2/people/mkramer/DUNE-TECH-2025/bkgdata_EH3.root
