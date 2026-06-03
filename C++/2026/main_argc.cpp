767f214813d61c1b18cd9f158d923ae40cbc471c
Ilker Parmaksiz <ilkerparmaksiz@windowslive.com>
2026-06-02 20:44:42 -0500

    Add files via upload
    C++ Examples

---
+++ C++/2026/main_argc.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(int argc,char *argv[]){
+
+    std::cout << "Hello, World! " << std::endl;
+    std::cout<<"Results -- > argc " <<argc << " argv " << argv[0] << " " << argv[1] <<std::endl;
+    return 0;
+}
